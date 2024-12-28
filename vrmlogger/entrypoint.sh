777e97ad2143e85886644014377f18febc3fc74a
kasper334 <15572556+kasper334@users.noreply.github.com>
2024-12-28 19:33:33 +0200

    Fix mk2-dbus container build

--- vrmlogger/entrypoint.sh
+++ vrmlogger/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir -p /data/conf /data/db
+mkdir -p /data/home /data/home/vnctunnel
 
 echo "VRM portal ID: $(ip link ls dev eth0 | grep ether | awk '{print $2}' | sed 's/://g')"
 echo
